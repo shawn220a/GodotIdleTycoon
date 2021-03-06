f7491989d6c0cb5030f1f03096906a60bef947f4
shawn220a <shawn.42496@gmail.com>
2021-03-06 15:46:21 -0600

    Create own method to update the money text

--- store.gd
+++ store.gd
@@ -5,9 +5,12 @@ var money = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MoneyLabel.text = "$ " + str(money)
-	
+	UpdateUI()
 
+func UpdateUI():
+	$MoneyLabel.text = "$ " + str(money)
 
 func _on_Button_pressed():
-	pass # Replace with function body.
+	print("You clicked the button")
+	money = money + 1
+	UpdateUI()

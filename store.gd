174cf24efb35682ca36859e03e59deaaf2bcf6b0
shawn220a <shawn.42496@gmail.com>
2021-03-06 16:15:43 -0600

    Create variables for store cost and profit

--- store.gd
+++ store.gd
@@ -3,6 +3,9 @@ extends Panel
 var money = 0
 var store_count = 0
 
+var store_cost = 5
+var store_profit = 2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	UpdateUI()
@@ -12,11 +15,11 @@ func UpdateUI():
 	$StoreCountLabel.text = "Store Count: " + str(store_count)
 
 func _on_ClickButton_pressed():
-	money += 1
+	money += store_profit
 	UpdateUI()
 
 func _on_BuyButton_pressed():
-	if money >= 5:
+	if money >= store_cost:
 		store_count += 1
-		money -= 5
+		money -= store_cost
 	UpdateUI()

ebffb59c104fa25e2f2976c94229990667ce8230
shawn220a <shawn.42496@gmail.com>
2021-03-06 15:57:16 -0600

    Create button functions for store count increase

--- store.gd
+++ store.gd
@@ -2,6 +2,7 @@ extends Panel
 
 # Declare member variables here. Examples:
 var money = 0
+var storeCount = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +10,12 @@ func _ready():
 
 func UpdateUI():
 	$MoneyLabel.text = "$ " + str(money)
+	$StoreCount.text = "Store Count: " + str(storeCount)
 
-func _on_Button_pressed():
+func _on_ClickButton_pressed():
 	money = money + 1
 	UpdateUI()
+
+func _on_BuyButton_pressed():
+	storeCount = storeCount + 1
+	UpdateUI()# Replace with function body.
